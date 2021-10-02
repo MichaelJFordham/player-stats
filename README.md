14e05fd71fb62293a6b9b4805355f6f5debe2c2d
Michael J. Fordham <michael97f@gmail.com>
2021-10-02 11:16:57 +0100

    Started initial work on player selection

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # player-stats
+
 A reusable football player statistics component.
+
+# Setup Instructions
+
+Install Live SASS Compiler from the VS Code extension store to compile SASS files to CSS live: https://marketplace.visualstudio.com/items?itemName=ritwickdey.live-sass
---
+++ index.js
@@ -0,0 +1,42 @@
+var playerInformation;
+var selectedPlayer;
+
+// Gets the player information from the local data file
+fetch("./data/player-stats.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => setPlayers(data));
+
+// Sets the players variable with the player information
+function setPlayers(playerData) {
+  playerInformation = playerData;
+  populateSelectOptions(playerData);
+}
+
+// Populates the select options based on the information from the player-stats.json file
+function populateSelectOptions(playerData) {
+  var playerSelect = document.getElementById("player-select");
+
+  playerData.players.forEach((person) => {
+    playerSelect.innerHTML += `<option value="${person.player.id}">${person.player.name.first} ${person.player.name.last}</option>`;
+  });
+}
+
+// Returns the currently selected player from the select element
+function setSelectedPlayer(playerSelectElement) {
+  selectedPlayer = getPlayerInformation(Number(playerSelectElement.value)); // Casts the selected value to a Number otherwise it is passed in as a string, which fails the strict equality check (===)
+  console.log(selectedPlayer);
+}
+
+// Returns the information for a player based on the player ID passed in
+function getPlayerInformation(playerId) {
+  for (let i = 0; i < playerInformation.players.length; i++) {
+    const person = playerInformation.players[i];
+
+    // If the IDs match, the matching person is returned
+    if (person.player.id === playerId) {
+      return person;
+    }
+  }
+}
